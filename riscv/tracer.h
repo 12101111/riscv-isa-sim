9307e6e063dc02c1e3483ad3649e1ad2d0b3caa9
12101111 <w12101111@gmail.com>
2025-12-07 23:32:28 +0800

    Fix counting

--- riscv/tracer.h
+++ riscv/tracer.h
@@ -4,20 +4,29 @@
 #define _RISCV_TRACER_H
 
 #include "processor.h"
+#define is(func) ((insn.bits() & MASK_##func) == MATCH_##func)
 
 static inline void trace_opcode(processor_t UNUSED *p, insn_bits_t UNUSED opc,
-                                insn_t UNUSED insn, reg_t UNUSED pc) {
+                                insn_t UNUSED insn, reg_t UNUSED pc, bool xlen32) {
   if (p->is_hooked(pc)) {
     switch (opc) {
-    case MATCH_C_J:
-    case MATCH_C_JR:
-#if xlen == 32
     case MATCH_C_JAL:
-#endif
-    case MATCH_C_JALR:
+      if (xlen32 && is(C_JAL))
+        p->branch_count++;
+      else
+        p->other_count++;
+      break;
+    case MATCH_C_J:
     case MATCH_JAL:
     case MATCH_JALR:
       p->branch_count++;
+      break;
+    case MATCH_C_JR:
+    case MATCH_C_JALR:
+      if (is(C_JR) || is(C_JALR))
+        p->branch_count++;
+      else
+        p->other_count++;
       break;
     case MATCH_C_BEQZ:
     case MATCH_C_BNEZ:
@@ -34,13 +43,6 @@ static inline void trace_opcode(processor_t UNUSED *p, insn_bits_t UNUSED opc,
     case MATCH_C_LBU:
     case MATCH_C_LHU:
     case MATCH_C_LH:
-#if xlen == 32
-    case MATCH_C_FLW:
-    case MATCH_C_FLWSP:
-#elif xlen == 64
-    case MATCH_C_LD:
-    case MATCH_C_LDSP:
-#endif
     case MATCH_C_FLD:
     case MATCH_C_FLDSP:
     case MATCH_CM_POP:
@@ -56,19 +58,26 @@ static inline void trace_opcode(processor_t UNUSED *p, insn_bits_t UNUSED opc,
     case MATCH_FLH:
     case MATCH_FLW:
     case MATCH_FLD:
+// #if xlen == 32
+    case MATCH_C_FLW:
+    case MATCH_C_FLWSP:
+// #elif xlen == 64
+    // case MATCH_C_LD:
+    // case MATCH_C_LDSP:
+// #endif
       p->load_count++;
       break;
     case MATCH_C_SW:
     case MATCH_C_SWSP:
     case MATCH_C_SB:
     case MATCH_C_SH:
-#if xlen == 32
+// #if xlen == 32
     case MATCH_C_FSW:
     case MATCH_C_FSWSP:
-#elif xlen == 64
-    case MATCH_C_SD:
-    case MATCH_C_SDSP:
-#endif
+// #elif xlen == 64
+    // case MATCH_C_SD:
+    // case MATCH_C_SDSP:
+// #endif
     case MATCH_C_FSD:
     case MATCH_C_FSDSP:
     case MATCH_CM_PUSH:
@@ -90,4 +99,5 @@ static inline void trace_opcode(processor_t UNUSED *p, insn_bits_t UNUSED opc,
   }
 }
 
+#undef is
 #endif
